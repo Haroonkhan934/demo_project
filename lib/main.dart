7e6a14801f1fc2be2db9d3c726edc7b906a6b29e
muhammad haroon <mharoon116011@gmail.com>
2025-02-13 23:36:10 -0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      appBar: AppBar(
-        backgroundColor: Colors.pinkAccent,
-        title: Text("I am a computer science student"),
-      ), //AppBar
-      body: const Center(
-        child: Image(
-          image: NetworkImage(
-              "https://images.pexels.com/photos/1704488/pexels-photo-1704488."
-              "jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
-        ), // Image
-      ), // Center
-    ), // Scaffold
-  ));
+  print("main");
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print("rebuild 3");
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.pinkAccent,
+          title: Text("My name is Muhammad Haroon"),
+        ),
+
+      ), // Scaffold
+    );
+  }
 }
