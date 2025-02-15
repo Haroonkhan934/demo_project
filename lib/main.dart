d9feb9c58326e8c44089639fb8d6da4ede771a9b
muhammad haroon <mharoon116011@gmail.com>
2025-02-14 23:04:09 -0800

    ok done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   print("main");
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,12 +13,49 @@ class MyApp extends StatelessWidget {
     print("rebuild 3");
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.tealAccent,
-        appBar: AppBar(
-          backgroundColor: Colors.pinkAccent,
-          title: Text("My name is Muhammad Haroon"),
-        ),
 
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title:const Text("App developer(MHK)"),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Row(
+                children: [
+                  Text("Muhammad haroon"),
+                  Icon(Icons.ac_unit_rounded),
+                ],
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.pink,
+                child:const Center(child: Text("Container 1")),
+          ),
+           const SizedBox(
+              height: 15,
+            ),
+            Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+            child:const Center(child: Text("Container 1")),
+          ),
+              const SizedBox(
+                height: 15,
+              ),
+          Container(
+          height: 100,
+          width: 100,
+          color: Colors.teal,
+          child:const Center(child: Text("Container 1")),
+                ),
+            ],
+          ),
+        ),
       ), // Scaffold
     );
   }
